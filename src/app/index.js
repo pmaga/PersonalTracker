11e3d8d6f43a66105a3640490b2f18d5a83607ec
Paweł Maga <pawel.p.maga@gmail.com>
2020-09-09 22:50:37 +0200

    refactoring

--- src/app/index.js
+++ src/app/index.js
@@ -4,21 +4,19 @@ Vue.component('status-edit', {
     },
     template: `
 <div>
-    <template>
-        <v-form ref="form">
-            <v-textarea outlined v-model="dayStatus.goals" label="Goals" :rows=4 required></v-textarea>
-            <v-textarea outlined v-model="dayStatus.completedTasks" label="Completed tasks" :rows=4 required></v-textarea>
-            <v-textarea outlined v-model="dayStatus.todos" label="Todo" :rows=4 required></v-textarea>
+    <v-form ref="form">
+        <v-textarea outlined v-model="dayStatus.goals" label="Goals" :rows=4></v-textarea>
+        <v-textarea outlined v-model="dayStatus.completedTasks" label="Completed tasks" :rows=4></v-textarea>
+        <v-textarea outlined v-model="dayStatus.todos" label="Todo" :rows=4></v-textarea>
 
-            <v-btn color="green" class="ma-2 white--text" @click="onSubmit">
-                Save 
-                <v-icon>mdi-content-save</v-icon>
-            </v-btn>
-            <v-btn color="red" class="ma-2 white--text" @click="onCancel">
-                Cancel 
-            </v-btn>
-        </v-form>
-    </template>
+        <v-btn color="green" class="ma-2 white--text" @click="onSubmit">
+            Save 
+            <v-icon>mdi-content-save</v-icon>
+        </v-btn>
+        <v-btn color="red" class="ma-2 white--text" @click="onCancel">
+            Cancel 
+        </v-btn>
+    </v-form>
 </div>`,
     data() { 
         return {
@@ -62,32 +60,27 @@ Vue.component('status-list', {
         statuses: Array
     },
     template: `
-    <div>
-        <template>
-            <v-card class="mx-auto" max-width="900" tile>
-                <v-list shaped>
-                <v-subheader>Statuses</v-subheader>
-                    <v-list-item-group color="primary">
-                        <v-list-item v-for="(item, index) in statuses" :key="item.date" @click="selectStatus(index)">
-                            <v-list-item-icon>
-                                <v-icon>mdi-calendar-plus</v-icon>
-                            </v-list-item-icon>
-                            <v-list-item-content>
-                                <v-list-item-title v-text="item.date"></v-list-item-title>
-                            </v-list-item-content>
-                        </v-list-item>
-                    </v-list-item-group>
-                </v-list>
-            </v-card>
-        </template>
-    </div>
+<div>
+    <v-card class="mx-auto" max-width="900" tile>
+        <v-list shaped>
+        <v-subheader>Statuses</v-subheader>
+            <v-list-item-group color="primary">
+                <v-list-item v-for="(item, index) in statuses" :key="item.date" @click="selectStatus(index)">
+                    <v-list-item-icon>
+                        <v-icon>mdi-calendar-plus</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                        <v-list-item-title v-text="item.date"></v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list-item-group>
+        </v-list>
+    </v-card>
+</div>
     `,
     methods: {
         selectStatus(index) {
             this.$emit('status-selected', this.statuses[index]);
-        },
-        editStatus(index, status) {
-            this.statuses[index] = status;
         }
     }
 });
@@ -97,31 +90,40 @@ var app = new Vue({
     vuetify: new Vuetify(),
     data: {
         selectedStatus: null,
-        statuses: null,
+        statuses: [],
         cacheKey: `statuses_${moment().month() + 1}`,
-        currentMonth: moment().format("MMMM YYYY")
+        today: moment()
     },
+
     mounted() {
         var item = localStorage.getItem(this.cacheKey);
         if (item) {
-          try {
+            try {
             this.statuses = JSON.parse(item);
-          } catch(ex) {
+            } catch(ex) {
             localStorage.removeItem(this.cacheKey);
-          }
+            }
         } else {
             this.statuses = [];
         }
-      },
+    },
+
+    computed: {
+        lastStatus() {
+            return this.statuses[this.statuses.length - 1];
+        }
+    },
+
     methods: {
-        statusSelected(status) {
+        selectStatus(status) {
+            console.log(status);
             this.selectedStatus = status;
         },
-        statusUpdated(status) {
+        updateStatus(status) {
             const idx = this.statuses.findIndex(element => element.date === status.date);
 
             if (idx === -1) {
-                this.statusAdded(status);
+                this.addStatus(status);
             }
             else {
                 Vue.set(this.statuses, idx, status);
@@ -130,31 +132,24 @@ var app = new Vue({
             this.selectedStatus = null;
             this.persistState();
         },
-        statusAdded(status) {
-            console.log(status);
+        addStatus(status) {
             this.statuses.push(status);
             this.persistState();
         },
-        addStatus() {
-            var lastStatus = this.statuses[this.statuses.length - 1];
-            var today = moment().format("DD-MM-YYYY");
+        createAndSelectNewStatus() {
+            const todayFormat = this.today.format("DD-MM-YYYY");
 
-            if (lastStatus && lastStatus.date === today) {
-                this.selectStatus = lastStatus;
+            if (this.lastStatus && this.lastStatus.date === todayFormat) {
+                this.selectStatus = this.lastStatus;
             } else {
                 this.selectedStatus = {
-                    date: today
+                    date: todayFormat
                 };
-                this.statusAdded(this.selectedStatus);
+                this.addStatus(this.selectedStatus);
             }
         },
         canAddNewStatus() {
-            if (!this.statuses || !this.statuses.length) {
-                return true;
-            }
-            var lastStatus = this.statuses[this.statuses.length - 1];
-            var today = moment().format("DD-MM-YYYY");
-            return lastStatus && lastStatus.date !== today;
+            return !this.lastStatus || this.lastStatus.date !== this.today.format("DD-MM-YYYY");
         },
         persistState() {
             const parsed = JSON.stringify(this.statuses);
